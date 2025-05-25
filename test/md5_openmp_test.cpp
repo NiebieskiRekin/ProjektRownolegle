142e21d5ae1966af6c0048c76a78dae3ae402382
niebieskirekin <blueshrk34@gmail.com>
2025-05-25 13:26:59 +0200

    improve tests

--- test/md5_openmp_test.cpp
+++ test/md5_openmp_test.cpp
@@ -2,27 +2,37 @@
 #include "utils.hpp"
 #include "md5_openmp.hpp"
 
-
-TEST(MD5Test, MD5Test2) {
+TEST(MD5OmpTest, MD5TestEmpty) {
     // Hashes empty string and stores signature
     void* sig = hash_openmp("");
-    // Test with cassert whether sig is correct from the expected value
-    assert(sig2hex(sig).compare("d41d8cd98f00b204e9800998ecf8427e") == 0);
- 
+    auto s = sig2hex(sig);
+    ASSERT_EQ(s.compare("d41d8cd98f00b204e9800998ecf8427e"), 0);
+}
+
+
+TEST(MD5OmpTest, MD5TestText) {
     // Hashes "The quick brown fox jumps over the lazy dog" and stores signature
     void* sig2 =hash_openmp("The quick brown fox jumps over the lazy dog");
     // Test with cassert whether sig is correct from the expected value
-    assert(sig2hex(sig2).compare("9e107d9d372bb6826bd81d3542a419d6") == 0);
- 
+    ASSERT_EQ(sig2hex(sig2).compare("9e107d9d372bb6826bd81d3542a419d6"), 0);
+}
+
+
+TEST(MD5OmpTest, MD5TestTextDot) {
     // Hashes "The quick brown fox jumps over the lazy dog." (notice the
     // additional period) and stores signature
+    void* sig2 = hash_openmp("The quick brown fox jumps over the lazy dog");
     void* sig3 = hash_openmp("The quick brown fox jumps over the lazy dog.");
     // Test with cassert whether sig is correct from the expected value
-    assert(sig2hex(sig3).compare("e4d909c290d0fb1ca068ffaddf22cbd0") == 0);
- 
+    ASSERT_EQ(sig2hex(sig3).compare("e4d909c290d0fb1ca068ffaddf22cbd0"), 0);
+    ASSERT_NE(sig2hex(sig3).compare(sig2hex(sig2)), 0);
+}
+
+
+TEST(MD5OmpTest, MD5TestAlphanumeric) { 
     // Hashes "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
     // and stores signature
     void* sig4 = hash_openmp("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
     // Test with cassert whether sig is correct from the expected value
-    assert(sig2hex(sig4).compare("d174ab98d277d9f5a5611c2c9f419d9f") == 0);
+    ASSERT_EQ(sig2hex(sig4).compare("d174ab98d277d9f5a5611c2c9f419d9f"), 0);
 }
--- test/md5_sequential_test.cpp
+++ test/md5_sequential_test.cpp
@@ -3,26 +3,36 @@
 #include "md5_sequential.hpp"
 
 
-TEST(MD5Test, MD5Test1) {
+TEST(MD5SequentialTest, MD5TestEmpty) {
     // Hashes empty string and stores signature
     void* sig = hash_sequential("");
-    // Test with cassert whether sig is correct from the expected value
-    assert(sig2hex(sig).compare("d41d8cd98f00b204e9800998ecf8427e") == 0);
- 
+    ASSERT_EQ(sig2hex(sig).compare("d41d8cd98f00b204e9800998ecf8427e"), 0);
+}
+
+
+TEST(MD5SequentialTest, MD5TestText) {
     // Hashes "The quick brown fox jumps over the lazy dog" and stores signature
     void* sig2 =hash_sequential("The quick brown fox jumps over the lazy dog");
     // Test with cassert whether sig is correct from the expected value
-    assert(sig2hex(sig2).compare("9e107d9d372bb6826bd81d3542a419d6") == 0);
- 
+    ASSERT_EQ(sig2hex(sig2).compare("9e107d9d372bb6826bd81d3542a419d6"), 0);
+}
+
+
+TEST(MD5SequentialTest, MD5TestTextDot) {
     // Hashes "The quick brown fox jumps over the lazy dog." (notice the
     // additional period) and stores signature
+    void* sig2 = hash_sequential("The quick brown fox jumps over the lazy dog");
     void* sig3 = hash_sequential("The quick brown fox jumps over the lazy dog.");
     // Test with cassert whether sig is correct from the expected value
-    assert(sig2hex(sig3).compare("e4d909c290d0fb1ca068ffaddf22cbd0") == 0);
- 
+    ASSERT_EQ(sig2hex(sig3).compare("e4d909c290d0fb1ca068ffaddf22cbd0"), 0);
+    ASSERT_NE(sig2hex(sig3).compare(sig2hex(sig2)), 0);
+}
+
+
+TEST(MD5SequentialTest, MD5TestAlphanumeric) { 
     // Hashes "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
     // and stores signature
     void* sig4 = hash_sequential("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
     // Test with cassert whether sig is correct from the expected value
-    assert(sig2hex(sig4).compare("d174ab98d277d9f5a5611c2c9f419d9f") == 0);
+    ASSERT_EQ(sig2hex(sig4).compare("d174ab98d277d9f5a5611c2c9f419d9f"), 0);
 }
