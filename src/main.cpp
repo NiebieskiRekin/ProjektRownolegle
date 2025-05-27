d5a769d03c8ad517f5569701809add9a7f5e118a
niebieskirekin <blueshrk34@gmail.com>
2025-05-27 19:35:06 +0200

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,36 @@
 #include <iostream>
+#include <vector>
 #include <omp.h>
 #include <cuda_runtime.h>
 
-int main() {
+// #include "md5_cuda.cuh"
+// #include "md5_kernels.cuh"
 
-    // int num_threads = omp_get_max_threads();
-    // std::cout << "Running with OpenMP, max threads: " << num_threads << std::endl;
+int main(int argc, char** argv) {
+    if (argc < 2){
+        std::cerr << "Za mało argumentów, użycie: main <CUDA|OPENCV|SEQUENTIAL> <pliki do przeliczenia>" << std::endl;
+        exit(1);
+    }
 
-    // int deviceCount = 0;
-    // cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
-    // if (error_id == cudaSuccess) {
-    //     std::cout << "CUDA devices found: " << deviceCount << std::endl;
-    // } else {
-    //     std::cerr << "CUDA error getting device count: " << cudaGetErrorString(error_id) << std::endl;
-    // }
+    std::string mode = argv[1];
+    std::vector<std::string> files;
+    files.reserve(argc-2);
+    for (int i=2; i<argc; i++){
+        files.push_back(argv[i]);
+    }
 
-    
+    if (mode == "CUDA"){
+        std::cout << "CUDA: " << std::endl;
+    } else if (mode == "OPENCV") {
+        std::cout << "OPENCV: " << std::endl;
+    } else {
+        std::cout << "SEQUENTIAL: " << std::endl;
+    }
+
+    for (const auto& file : files){
+        std::cout << file << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }
